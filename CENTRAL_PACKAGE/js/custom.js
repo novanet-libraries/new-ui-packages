171896a264ff68d9db0ed2eb6a4944b1d4ebfe2d
Brad Dewar <brad.dewar@novanet.ns.ca>
2017-09-11 11:22:03 -0300

    typo

--- CENTRAL_PACKAGE/js/custom.js
+++ CENTRAL_PACKAGE/js/custom.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  var app = angular.module('centralCustom', []);
+  var app = angular.module('centralCustom', ['angularLoad']);
 
 //  app.component('prmSpinnerAfter', {
 //    template: '<strong>SPINNER-AFTER</strong>'
@@ -244,7 +244,59 @@
 
     };
   });
-  
+
+  //define a <live-help-card> component that draws an <md-card> with "Ask a Librarian" in it.
+  //the controller pulls in the requisite LibraryH3lp code to make it work.
+  app.component('liveHelpCard', {
+    controller: 'LiveHelpController2',
+    templateUrl: 'custom/CENTRAL_PACKAGE/html/live-help-card.html'
+  })
+  .controller('LiveHelpController2', ['$scope', 'angularLoad', function($scope, angularLoad){
+    var ctrl       = this,
+        chatWindow = null,
+        chatUrl    = (function(){
+          var url      = 'https://libraryh3lp.com/chat/novanet@chat.libraryh3lp.com',
+              skin     = '10162',
+              theme    = 'gota',
+              title    = 'Live Help',
+              identity = 'Librarian',
+              css      = 'https://sites.stfx.ca/library/sites/sites.stfx.ca.library/files/libraryh3lp.css';
+
+          url += '?skin='     + encodeURIComponent(skin);
+          url += '&theme='    + encodeURIComponent(theme);
+          url += '&title='    + encodeURIComponent(title);
+          url += '&identity=' + encodeURIComponent(identity);
+          url += '&css='      + encodeURIComponent(css);
+          return url;
+        })();
+
+    ctrl.$onInit = function (){
+      angularLoad.loadScript(
+        'https://libraryh3lp.com/js/libraryh3lp.js?multi,poll'
+      ).then(
+        function(){
+          //console.log('Loaded external libraryh3lp script.');
+        },
+        function(data){
+          console.error('Failed to load external libraryh3lp script.');
+          console.error(JSON.stringify(data, null, 2));
+      });
+    };
+
+    $scope.liveHelpText = "Ask A Librarian!";
+
+    $scope.showChat = function(evt){
+      evt && evt.stopPropagation();
+
+      if (chatWindow && !chatWindow.closed){
+        chatWindow.focus();
+      }
+      else{
+        chatWindow = window.open(chatUrl, 'Live Help', 'resizable=1,width=300,height=300,top:100,left:100');
+      }
+    };
+
+  }]); //end definition of LiveHelpController
 
 /*
   //make a <novanet-library-hours> component, institutions can put it on the landing page (or anywhere, or nowhere)
