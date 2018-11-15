c849a02eabf574180e1a5d2510f67c7c4d9f10e2
Brad Dewar <brad.dewar@novanet.ca>
2018-11-15 15:47:06 -0400

    eol/whitespace

--- SMU/js/custom.js
+++ SMU/js/custom.js
@@ -6,29 +6,29 @@
   window.browzine = {
     libraryId:  "SMU",
     apiKey:     "ENTER API KEY",
- 
+
     journalCoverImagesEnabled: true,
- 
+
     journalBrowZineWebLinkTextEnabled: true,
     journalBrowZineWebLinkText: "View Journal Contents",
- 
+
     acticleBrowZineWebLinkTextEnabled: true,
     articleBrowZineWebLinkText: "View Issue Contents",
- 
+
     articlePDFDownloadLinkEnabled: true,
     articlePDFDownloadLinkText: "Download PDF",
 
     printRecordsIntegrationEnabled: true,
   };
- 
+
   browzine.script = document.createElement("script");
   browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
   document.head.appendChild(browzine.script);
- 
+
   app.controller('BrowzineAfterAvailabilityController', function($scope) {
     window.browzine.primo.searchResult($scope);
   });
- 
+
   app.component('prmSearchResultAvailabilityLineAfter', {
     bindings: { parentCtrl: '<' },
     controller: 'BrowzineAfterAvailabilityController'
