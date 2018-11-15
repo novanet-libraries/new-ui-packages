071b648a96c86247f9ec514eeb67d7286b49455c
Brad Dewar <brad.dewar@novanet.ca>
2018-11-15 14:53:27 -0400

    copy/paste example code from vendor

---
+++ SMU/js/custom.js
@@ -0,0 +1,37 @@
+(function () {
+  "use strict";
+
+  var app = angular.module('viewCustom', ['angularLoad']);
+
+  window.browzine = {
+    libraryId: "SMU",
+    apiKey: "ENTER API KEY",
+ 
+    journalCoverImagesEnabled: true,
+ 
+    journalBrowZineWebLinkTextEnabled: true,
+    journalBrowZineWebLinkText: "View Journal Contents",
+ 
+    acticleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkText: "View Issue Contents",
+ 
+    articlePDFDownloadLinkEnabled: true,
+    articlePDFDownloadLinkText: "Download PDF",
+ 
+    printRecordsIntegrationEnabled: true,
+  };
+ 
+  browzine.script = document.createElement("script");
+  browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
+ 
+  app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+ 
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmSearchResultAvailabilityLineAfterController'
+  });
+
+})();
