63a27e05e49b5e3513321a9e472c66c1e8f040f7
Brad Dewar <brad.dewar@novanet.ca>
2019-01-03 16:03:47 -0400

    redirect all requests to nice hostname
    SAML / ADFS is set up to expect all auth requests to come from acadia.novanet.ca.  Requests from novanet-primo.hosted.exlibrisgroup.com will fail.

---
+++ ACAD/js/custom.js
@@ -0,0 +1,11 @@
+if (window.location.hostname == 'novanet-primo.hosted.exlibrisgroup.com'){
+  location.href = location.href.replace('novanet-primo.hosted.exlibrisgroup.com', 'acadia.novanet.ca');
+}
+/*
+(function () {
+  "use strict";
+  
+  var app = angular.module('viewCustom', []);
+
+})();
+*/
