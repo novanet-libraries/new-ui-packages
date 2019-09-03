34020e9e610f341da5115369322e7e335488cce8
Brad Dewar <brad.dewar@novanet.ca>
2019-09-03 13:39:11 -0300

    move if-not-MTA logic

--- CENTRAL_PACKAGE/js/custom.js
+++ CENTRAL_PACKAGE/js/custom.js
@@ -82,10 +82,10 @@
     template: '<seasonal-notice-card></seasonal-notice-card>'
   });
   app.component('prmTopbarAfter', {
-    //template: '<live-help-widget></live-help-widget>'
-    templateUrl: function(){
-      return window.appConfig.vid == 'MTA' ? '' : 'custom/CENTRAL_PACKAGE/html/live-help-widget.html';
-    }
+    template: '<live-help-widget></live-help-widget>'
+    //templateUrl: function(){
+    //  return window.appConfig.vid == 'MTA' ? '' : 'custom/CENTRAL_PACKAGE/html/live-help-widget.html';
+    //}
   });
 
   //add "try this search elsewhere" options
