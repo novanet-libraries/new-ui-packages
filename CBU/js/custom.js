39a6ac3107ba002b8847eadab6d7af2c5428d6e2
Brad Dewar <brad.dewar@novanet.ns.ca>
2022-02-15 15:13:03 -0400

    add CBU's Browzine trial to Primo

---
+++ CBU/js/custom.js
@@ -0,0 +1,54 @@
+(function () {
+  "use strict";
+
+  var app = angular.module('viewCustom', []);
+
+  window.browzine = {
+    libraryId:  "206",
+    apiKey:     "87756e4c-c0af-49dd-8964-5c8960c43f66",
+
+    journalCoverImagesEnabled: true,
+
+    journalBrowZineWebLinkTextEnabled: true,
+    journalBrowZineWebLinkText: "View Journal Contents",
+
+    articleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkText: "View Issue Contents",
+
+    articlePDFDownloadLinkEnabled: true,
+    articlePDFDownloadLinkText: "Download PDF",
+
+    articleLinkEnabled: true,
+    articleLinkText: "Read Article",
+
+    printRecordsIntegrationEnabled: true,
+
+    unpaywallEmailAddressKey: "jasmine_hoover@cbu.ca",
+
+    articlePDFDownloadViaUnpaywallEnabled: true,
+    articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)",
+
+    articleLinkViaUnpaywallEnabled: true,
+    articleLinkViaUnpaywallText: "Read Article (via Unpaywall)",
+
+    articleAcceptedManuscriptPDFViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptPDFViaUnpaywallText: "Download PDF (Accepted Manuscript via Unpaywall)",
+
+    articleAcceptedManuscriptArticleLinkViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptArticleLinkViaUnpaywallText: "Read Article (Accepted Manuscript via Unpaywall)",
+  };
+
+  browzine.script = document.createElement("script");
+  browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
+
+  app.controller('BrowzineAfterAvailabilityController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'BrowzineAfterAvailabilityController'
+  });
+
+})();
