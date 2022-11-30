3f4916576aa00bfd39da744b021d7b9e1256cd30
Brad Dewar <brad.dewar@novanet.ns.ca>
2022-11-30 10:26:22 -0400

    fix path to template html file

--- 01NOVA_MSVU-TEST/js/custom.js
+++ 01NOVA_MSVU-TEST/js/custom.js
@@ -4,7 +4,7 @@
   var app = angular.module('viewCustom', []);
 
   app.component('prmSearchBarAfter', {
-    templateUrl: 'custom/CENTRAL_PACKAGE/html/seasonal-alert-bar_en_US.html';
+    templateUrl: 'custom/01NOVA_NETWORK-CENTRAL_PACKAGE/html/seasonal-alert-bar_en_US.html';
   });
 
 })();
