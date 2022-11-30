027c8f044e60ef27915509acca6632d6ffd84941
Brad Dewar <brad.dewar@novanet.ns.ca>
2022-11-30 10:20:16 -0400

    add seasonal search bar to TEST view

---
+++ 01NOVA_MSVU-TEST/js/custom.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  var app = angular.module('viewCustom', []);
+
+  app.component('prmSearchBarAfter', {
+    templateUrl: 'custom/CENTRAL_PACKAGE/html/seasonal-alert-bar_en_US.html';
+  });
+
+})();
