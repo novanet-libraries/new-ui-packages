8f2d7396683a143f6376d5a896a36551b63a0d58
Brad Dewar <brad.dewar@novanet.ca>
2023-08-14 13:50:34 -0300

    Update custom.js

--- 01NOVA_NETWORK-CENTRAL_PACKAGE/js/custom.js
+++ 01NOVA_NETWORK-CENTRAL_PACKAGE/js/custom.js
@@ -452,4 +452,38 @@
   }); // end LibraryHours component definition.
 
 
+  //availability line fallback message
+  //  Occasionally (often with recently removed ebooks) the calculated availability
+  //  statement is missing/blank.   This is a temporary placeholder for that.
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: {
+      parentCtrl: '<'
+    },
+    controller: 'AvailFallbackMsg'
+  }).controller('AvailFallbackMsg', ['$element', function($element){
+    var vm = this;
+    vm.$onInit = function(){
+      if (!vm.parentCtrl.displayedAvailability || vm.parentCtrl.displayedAvailability.length < 1){
+        if ($element.text().replace(/\s+/g, '') == '') {
+          console.error('missing availability stmt in result: ', vm.parentCtrl.result["@id"]);
+          //vm.parentCtrl.displayedAvailability = [ 'error_not_assigned' ];
+          $element.css('color', '#6c6161').text('Not Available');
+        }
+      }else{
+        $element.text(' ');
+      }
+    };
+    vm.$doCheck = function(){
+      if (!vm.parentCtrl.displayedAvailability || vm.parentCtrl.displayedAvailability.length < 1){
+        if ($element.text().replace(/\s+/g, '') == '') {
+          console.error('missing availability stmt in result: ', vm.parentCtrl.result["@id"]);
+          //vm.parentCtrl.displayedAvailability = [ 'error_not_assigned' ];
+          $element.css('color', '#6c6161').text('Not Available');
+        }
+      }else{
+        $element.text(' ');
+      }
+    };    
+  }]);
+  
 })();
