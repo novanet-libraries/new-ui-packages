0561b6660f136466eccf2dfe4b5640e0ce19d3e5
Brad Dewar <brad.dewar@novanet.ca>
2023-09-07 13:19:33 -0300

    add 'report a problem' button in Action area

--- 01NOVA_NETWORK-CENTRAL_PACKAGE/js/custom.js
+++ 01NOVA_NETWORK-CENTRAL_PACKAGE/js/custom.js
@@ -126,7 +126,17 @@
       }
     }
   });
-  
+
+  app.component('reportAProblem', {
+    templateUrl: function(){
+      var lang = window.appConfig['primo-view']['attributes-map'].interfaceLanguage == 'fr' ? 'fr_FR' : 'en_US';
+      return 'custom/' + window.appConfig.vid.replace(':', '-') + '/html/report-a-problem_' + lang + '.html';
+    }
+  });
+  app.component('prmActionListAfter', {
+    template: '<report-a-problem></report-a-problem>'
+  });
+
   app.component('seasonalNoticeCard', {
     templateUrl: function(){
       var lang = window.appConfig['primo-view']['attributes-map'].interfaceLanguage == 'fr' ? 'fr_FR' : 'en_US';
